bae9d38fdbefe66e9ce134f0fbfee92ea9fd6fc2
Sören <soeren.etler@gmail.com>
2020-07-02 18:15:55 +0200

    use own CD pipeline

---
+++ vue.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  pluginOptions: {
+    electronBuilder: {
+      builderOptions: {
+        linux: {
+          target: 'AppImage',
+          category: 'Presentation',
+          publish: 'github'
+        },
+        win: {
+          publish: 'github',
+          target: 'portable'
+        }
+
+        // options placed here will be merged with default configuration and passed to electron-builder
+      }
+    }
+  }
+}
